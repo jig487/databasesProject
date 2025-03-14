91ba6cda99dbbf4b6d15ce281bd2c8b277e09849
BROCK HANSEN <brockhansen56@gmail.com>
2025-03-14 15:11:21 -0500

    update

--- website/my-react-app/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to My React Website</h1>
-      <p>This is a simple React app!</p>
-    </div>
-  );
-}
