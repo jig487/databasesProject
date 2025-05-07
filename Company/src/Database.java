d716096a2c89cf4ea6bad4ae74de229a6bc015b4
LiverMSE <ethanwateski@gmail.com>
2025-05-07 16:50:25 -0500

    added all inserts 'Create'

--- Company/src/Database.java
+++ Company/src/Database.java
@@ -55,12 +55,6 @@ public class Database {
 	}
 
 	public ResultSet genreLookup(int gid) throws SQLException {
-		if (gid == -1) {
-			String query = "SELECT * FROM Genre";
-			PreparedStatement stmt = connection.prepareStatement(query);
-			ResultSet results = stmt.executeQuery();
-			return results;			
-		}
 		String query = "SELECT * FROM Genre WHERE GID = ?";
 		PreparedStatement stmt = connection.prepareStatement(query);
 		stmt.setInt(1, gid);
@@ -84,18 +78,136 @@ public class Database {
 		stmt.setDouble(3, e.getRating());
 		stmt.setInt(4, e.getRatingCount());
 		stmt.setInt(5, e.getPages());
-		stmt.setInt(5, e.getPublishYear());
-		int numRowsAffected = stmt.executeUpdate();
-		insertIsGenre(e);
-		System.out.println("Number of rows affected: " + numRowsAffected);
+		stmt.setInt(6, e.getPublishYear());
+		stmt.executeUpdate();
 	}
 
-	public void insertIsGenre(Book e) {
-		try {
-			ResultSet result = genreLookup(-1);
-		Finally 
-		
+	public int insertAuthor(String authorName) throws SQLException {
+		String checkSql = "SELECT AID FROM Author WHERE AuthorName = ?";
+		PreparedStatement checkStmt = connection.prepareStatement(checkSql);
+		checkStmt.setString(1, authorName);
+		ResultSet rs = checkStmt.executeQuery();
+		if (rs.next()) {
+			return rs.getInt("AID");
 		}
+		String maxSql = "SELECT MAX(AID) AS maxAid FROM Author";
+		PreparedStatement maxStmt = connection.prepareStatement(maxSql);
+		ResultSet maxRs = maxStmt.executeQuery();
+		int newAid = 1;
+		if (maxRs.next() && maxRs.getInt("maxAid") != 0) {
+			newAid = maxRs.getInt("maxAid") + 1;
+		}
+		String insertSql = "INSERT INTO Author (AID, AuthorName) VALUES (?, ?)";
+		PreparedStatement insertStmt = connection.prepareStatement(insertSql);
+		insertStmt.setInt(1, newAid);
+		insertStmt.setString(2, authorName);
+		insertStmt.executeUpdate();
+		return newAid;
+	}
+
+	public int insertGenre(String genreName) throws SQLException {
+		String checkSql = "SELECT GID FROM Genre WHERE GenreName = ?";
+		PreparedStatement checkStmt = connection.prepareStatement(checkSql);
+		checkStmt.setString(1, genreName);
+		ResultSet rs = checkStmt.executeQuery();
+		if (rs.next()) {
+			return rs.getInt("GID");
+		}
+		String maxSql = "SELECT MAX(GID) AS maxGid FROM Genre";
+		PreparedStatement maxStmt = connection.prepareStatement(maxSql);
+		ResultSet maxRs = maxStmt.executeQuery();
+		int newGid = 1;
+		if (maxRs.next() && maxRs.getInt("maxGid") != 0) {
+			newGid = maxRs.getInt("maxGid") + 1;
+		}
+		String insertSql = "INSERT INTO Genre (GID, GenreName) VALUES (?, ?)";
+		PreparedStatement insertStmt = connection.prepareStatement(insertSql);
+		insertStmt.setInt(1, newGid);
+		insertStmt.setString(2, genreName);
+		insertStmt.executeUpdate();
+		return newGid;
+	}
+
+	public int insertPatron(String firstName, String lastName) throws SQLException {
+		String checkSql = "SELECT PID FROM Patron WHERE FirstName = ? AND LastName = ?";
+		PreparedStatement checkStmt = connection.prepareStatement(checkSql);
+		checkStmt.setString(1, firstName);
+		checkStmt.setString(2, lastName);
+		ResultSet rs = checkStmt.executeQuery();
+		if (rs.next()) {
+			return rs.getInt("PID");
+		}
+		String maxSql = "SELECT MAX(PID) AS maxPid FROM Patron";
+		PreparedStatement maxStmt = connection.prepareStatement(maxSql);
+		ResultSet maxRs = maxStmt.executeQuery();
+		int newPid = 1;
+		if (maxRs.next() && maxRs.getInt("maxPid") != 0) {
+			newPid = maxRs.getInt("maxPid") + 1;
+		}
+		String insertSql = "INSERT INTO Patron (PID, FirstName, LastName) VALUES (?, ?, ?)";
+		PreparedStatement insertStmt = connection.prepareStatement(insertSql);
+		insertStmt.setInt(1, newPid);
+		insertStmt.setString(2, firstName);
+		insertStmt.setString(3, lastName);
+		insertStmt.executeUpdate();
+		return newPid;
+	}
+
+	public int insertOwner(String firstName, String lastName) throws SQLException {
+		String checkSql = "SELECT OID FROM Owner WHERE FirstName = ? AND LastName = ?";
+		PreparedStatement checkStmt = connection.prepareStatement(checkSql);
+		checkStmt.setString(1, firstName);
+		checkStmt.setString(2, lastName);
+		ResultSet rs = checkStmt.executeQuery();
+		if (rs.next()) {
+			return rs.getInt("OID");
+		}
+		String maxSql = "SELECT MAX(OID) AS maxOid FROM Owner";
+		PreparedStatement maxStmt = connection.prepareStatement(maxSql);
+		ResultSet maxRs = maxStmt.executeQuery();
+		int newOid = 1;
+		if (maxRs.next() && maxRs.getInt("maxOid") != 0) {
+			newOid = maxRs.getInt("maxOid") + 1;
+		}
+		String insertSql = "INSERT INTO Owner (OID, FirstName, LastName) VALUES (?, ?, ?)";
+		PreparedStatement insertStmt = connection.prepareStatement(insertSql);
+		insertStmt.setInt(1, newOid);
+		insertStmt.setString(2, firstName);
+		insertStmt.setString(3, lastName);
+		insertStmt.executeUpdate();
+		return newOid;
+	}
+	
+	public void insertWrote(String isbn, int aid) throws SQLException {
+		String sql = "INSERT INTO Wrote (ISBN, AID) VALUES (?, ?)";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, isbn);
+		stmt.setInt(2, aid);
+		stmt.executeUpdate();
+	}
+
+	public void insertIsGenre(String isbn, int gid) throws SQLException {
+		String sql = "INSERT INTO IsGenre (ISBN, GID) VALUES (?, ?)";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, isbn);
+		stmt.setInt(2, gid);
+		stmt.executeUpdate();
+	}	
+
+	public void insertBorrows(String isbn, int pid) throws SQLException {
+		String sql = "INSERT INTO Borrows (ISBN, PID) VALUES (?, ?)";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, isbn);
+		stmt.setInt(2, pid);
+		stmt.executeUpdate();
+	}
+
+	public void insertOwns(String isbn, int oid) throws SQLException {
+		String sql = "INSERT INTO Owns (ISBN, AID) VALUES (?, ?)";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, isbn);
+		stmt.setInt(2, oid);
+		stmt.executeUpdate();
 	}
 	
 	public void updateRating(Book e, double rating) throws SQLException {
